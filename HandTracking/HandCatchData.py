d2f3c3ee3b5094eb441b1ce653233aa458766226
sAto-3 <17214@fukushima.kosen-ac.jp>
2021-07-07 17:09:03 +0900

    20210707-2 閲覧できる感じになった

--- HandTracking/HandCatchData.py
+++ HandTracking/HandCatchData.py
@@ -53,6 +53,7 @@ while True:
         # 指をおろしている判定を取得
         checkedList = detector.checkFinger()
         # print(checkedList)
+        # print(len(lmlist))
 
     # 画像の表示
     cv2.imshow("Image", img)
@@ -66,19 +67,12 @@ while True:
     # 1キーで座標を登録
     elif k == 49:
         # データの追加
-        HandData.append([])
-        # データの入力
-        for hand in range(len(lmlist)):
-            HandData[time].append([])
-            HandData[time][hand].append([row[1] for row in lmlist[hand]])
-            HandData[time][hand].append([row[2] for row in lmlist[hand]])
-            HandData[time][hand].append([row[3] for row in lmlist[hand]])
+        HandData.append(lmlist)
         time += 1
         print("SAVEING")
     # 2キーを押したら初期化
     elif k == 50:
         HandData = []
-        time = 0
 print(HandData)
 # [frame][hand][xyz][id]
 # 保存した座標を表示
@@ -90,24 +84,31 @@ if HandData:
         for hand in range(len(HandData[frame])):
             for id in range(21):
                 if id % 4 == 0:
-                    ax.scatter(HandData[frame][hand][0][id], HandData[frame][hand]
-                               [1][id], HandData[frame][hand][2][id], color="#DD0000")
+                    ax.scatter(HandData[frame][hand][id][1], HandData[frame][hand]
+                               [id][2], HandData[frame][hand][id][3], color="#DD0000")
                 elif id % 4 == 1:
-                    ax.scatter(HandData[frame][hand][0][id], HandData[frame][hand]
-                               [1][id], HandData[frame][hand][2][id], color="#AA4400")
+                    ax.scatter(HandData[frame][hand][id][0], HandData[frame][hand]
+                               [id][2], HandData[frame][hand][id][3], color="#AA4400")
                 elif id % 4 == 2:
-                    ax.scatter(HandData[frame][hand][0][id], HandData[frame][hand]
-                               [1][id], HandData[frame][hand][2][id], color="#778800")
+                    ax.scatter(HandData[frame][hand][id][0], HandData[frame][hand]
+                               [id][2], HandData[frame][hand][id][3], color="#778800")
                 elif id % 4 == 3:
-                    ax.scatter(HandData[frame][hand][0][id], HandData[frame][hand]
-                               [1][id], HandData[frame][hand][2][id], color="#33CC00")
+                    ax.scatter(HandData[frame][hand][id][0], HandData[frame][hand]
+                               [id][2], HandData[frame][hand][id][3], color="#33CC00")
         plt.show()
+    
     #　numpy.save でnpy形式で出力
-    time = 0
-    np.save("NPY/HandData{0}.npy".format(str(time)), HandData)
+    # time = 0
+    # np.save("NPY/HandData{0}.tsv".format(str(time)), HandData,delimiter="t")
 
+with open("NPY/HandData{0}.tsv".format(str(time)),"w") as outfile:
+    for slice3D in HandData:
+        for slice2D in slice3D:
+            np.savetxt(outfile,slice2D)
+            outfile.write('# New 2Dslice\n')
+        outfile.write('# New 3Dslice\n')
     # json形式で出力
-    with open("JSON/HandData{0}.json", 'w') as f:
+    with open("JSON/HandData{0}.json".format(str(time)), 'w') as f:
         l = HandData.tolist()
         s = json.dumps(l, indent=4)
         f.write(s)
